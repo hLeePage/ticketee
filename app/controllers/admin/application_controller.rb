4816156bb9624f8754ffc80dbf5cf4b6b4824a6d
hLeePage <hLeepage@yahoo.com>
2015-12-31 15:06:56 -0600

    Verify use of authorization in all controllers

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ApplicationController < ApplicationController
-
+  skip_after_action :verify_authorized, :verify_policy_scoped
   before_action :authorize_admin!
 
   def index
@@ -13,5 +13,5 @@ class Admin::ApplicationController < ApplicationController
       redirect_to root_path, alert: "You must be an admin to do that."
     end
   end
-  
+
 end
