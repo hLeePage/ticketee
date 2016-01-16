d2413ec3b24c665d871f0250b18fd20b687b05a6
hLeePage <hLeepage@yahoo.com>
2016-01-15 22:38:40 -0600

    Add ability for admins to set a default state for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,24 @@ Rails.application.routes.draw do
     root "application#index"
 
     resources :projects, only: [:new, :create, :destroy]
+
     resources :users do
       member do
         patch :archive
       end
     end
-    resources :states, only: [:index, :new, :create]
+
+    resources :states, only: [:index, :new, :create] do
+      member do
+        get :make_default
+      end
+    end
   end
 
+############################
   devise_for :users
+############################
+
   root "projects#index"
 
   resources :projects, only: [:index, :show, :edit, :update] do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115221613) do
+ActiveRecord::Schema.define(version: 20160116042436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,9 @@ ActiveRecord::Schema.define(version: 20160115221613) do
   create_table "states", force: :cascade do |t|
     t.string   "name"
     t.string   "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "default",    default: false
   end
 
   create_table "tickets", force: :cascade do |t|
