0ea50797a013dc15bdf1ade20fb314c234779d4f
hLeePage <hLeepage@yahoo.com>
2016-01-16 13:11:36 -0600

    Add ability for users to add tags when creating a comment

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -21,7 +21,6 @@ RSpec.feature "Users can comment on tickets" do
       end
     end
 
-
   scenario "with invalid attributes" do
     visit project_ticket_path(project, ticket)
     click_button "Create Comment"
@@ -50,5 +49,19 @@ RSpec.feature "Users can comment on tickets" do
     assign_role!(user, :editor, project)
     visit project_ticket_path(project, ticket)
     expect(page).not_to have_select "State"
+  end
+
+  scenario "when adding a new tag to a ticket" do
+    visit project_ticket_path(project, ticket)
+    expect(page).not_to have_content "bug"
+
+    fill_in "Text", with: "Adding the bug tag"
+    fill_in "Tags", with: "bug"
+    click_button "Create Comment"
+
+    expect(page).to have_content "Comment has been created."
+    within("#ticket #tags") do
+      expect(page).to have_content "bug"
+    end
   end
 end
