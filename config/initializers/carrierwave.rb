8cc8a7dfaddd9b1f0515fde189907479cd305c93
hLeePage <hLeepage@yahoo.com>
2016-01-17 22:32:07 -0600

    Experimenting with the not-working Fog gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,8 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root
-  
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["S3_KEY"],
-    aws_secret_access_key: ENV["S3_SECRET"],
-    region: ENV["S3_REGION"]
-    }
-    config.fog_directory = ENV["S3_BUCKET"]
-  else
-  config.storage = :file
-  end
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]
+  }
+  config.fog_directory  = ENV["AWS_BUCKET"]
 end
